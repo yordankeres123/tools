f20d4d4a73f725e28eccbbc53a299c1978afdbc7
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-28 12:14:28 +0700

    Add files via upload

---
+++ toolsv4.sh
@@ -0,0 +1,239 @@
+clear
+figlet LOGIN | lolcat
+echo "##########################################" | lolcat
+echo "######### I'M BIGBOS STARLA###############" | lolcat
+echo "#######BIASA DISEBUT GREEN DRAGON#########" | lolcat
+echo "##########################################" | lolcat
+sleep 3
+toilet -f big -F gay LOGIN
+sleep 3
+clear
+echo "##########################################"
+echo "############### KONTAK SAYA###############" | lolcat
+echo "#########EFBE GUE Bigbos Starla###########"
+echo "############IGE GUE bigbosstarla##########" | lolcat
+echo "##########################################"
+echo
+echo "Login dulu Yah Broo"
+echo "Kalau Gak Tau Kodenya Tanya saya Bro" | lolcat
+echo "Untuk Lanjut ke Toolsv4"
+echo
+echo "12.  72.  02.  73.  00.  99." | lolcat
+echo "66.  77.  88.  11.  22.  33."
+echo "44.  55.  10.  91.  83.  09." | lolcat
+echo
+echo "121212. keluar "
+echo
+echo "SILAHKAN PILIH KODENYA"
+read -p "PILIH KODENYA $" koid
+
+if [ $koid = 12 ] || [ $koid = 12. ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 72 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 02 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 73 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 00 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 99 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 66 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 77 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 88 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 11 ]
+then
+clear
+figlet BERHASIL 
+echo "SAYA GREEN DRAGON" | lolcat
+echo "Loading"
+apt update
+apt upgrade
+pkg install python
+pkg install git
+git clone https://github.com/yordankeres123/toolsev4
+cd toolev4
+sh tools.sh
+fi
+
+if [ $koid = 22 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 33 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 44 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 55 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 10 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 91 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 83 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 09 ]
+then
+clear
+toilet -f big -F gay GAGAL | lolcat
+echo "SALAH GOBLOK KODENYA PM GUE KALAU"
+echo "GAK TAU KONTAK ADA DIATAS" | lolcat
+sleep 3
+cd tools
+sh toolsv4.sh
+fi
+
+if [ $koid = 121212 ]
+then
+clear
+figlet kluar
+echo "KLUAR DARI TOOLS"
+sleep 2
+exit
+fi
